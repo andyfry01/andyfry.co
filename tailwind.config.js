905ac0dd77614eec7a654694222b9eda419beacb
Andy <andyfry01@gmail.com>
2021-08-30 16:15:18 -0600

    Stop typography classes from styling code examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,24 @@ module.exports = {
   purge: {
     mode: "all",
     enabled: true,
-    content: ["./_posts/**/*.md", "./_includes/**/*.html", "./_layouts/**/*.html", "./about.md", "./index.html"],
+    content: ["./_posts/**/*.md", "./_includes/**/*.html", "./_layouts/**/*.html", "./about.md", "./index.html", "./public/**/*.mjs"],
   },
   plugins: [
     require('@tailwindcss/typography')
-  ]
+  ],
+  theme: {
+    extend: {
+      typography: {
+        default: {
+          css: {
+            pre: false,
+            code: false,
+            'pre code': false,
+            'code::before': false,
+            'code::after': false
+          }
+        }
+      }
+    }
+  }
 };
